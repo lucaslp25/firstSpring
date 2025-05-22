d22da2a7b089839da2bb3aa78249842f596e5542
Lucas Lopes <lp.skt25@gmail.com>
2025-05-22 18:32:09 -0300

    Product adicionado ao projeto

---
+++ src/main/java/com/lucaslp/firstSpring/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.lucaslp.firstSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucaslp.firstSpring.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{ //SpringData JPA
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
