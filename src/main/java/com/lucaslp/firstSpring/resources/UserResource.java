20aeb981d19aa90ac2fcac18a2dbd43a355f0bf5
Lucas Lopes <lp.skt25@gmail.com>
2025-05-24 09:32:56 -0300

    exceções personalizadas adicionada para as requisições

--- src/main/java/com/lucaslp/firstSpring/resources/UserResource.java
+++ src/main/java/com/lucaslp/firstSpring/resources/UserResource.java
@@ -1,19 +1,25 @@
 package com.lucaslp.firstSpring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucaslp.firstSpring.entities.User;
 import com.lucaslp.firstSpring.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users") //caminho do recurso
+@RequestMapping(value = "/users") //caminho do recurso (ENDPOINT...) DIGITAMOS no localhost ou no servidor que esta e ele retorna a requisição de tudo que tem a partir desta classe...
 public class UserResource {
 
 	@Autowired
@@ -27,10 +33,31 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}") //ENDPOINT adicional ao request mapping, ao adicionar uma / + um id de da entidade estaremos buscando um usuario em especico e não todos!
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping 
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 	
 }
